7d732de48e00f91af41c25fed3efbc64ad9a033b
Sofia Campos de Carvalho <sofitch06@gmail.com>
2023-06-10 03:03:04 +0100

    Solver and refactor

--- main.cpp
+++ main.cpp
@@ -1,40 +1,89 @@
 #include <iostream>
 #include <vector>
+#include <array>
 #include <algorithm>
 #include <ctime>
 #include <random>
 
-// TODO remove aux functions from inside class, refacor functions, refactor expressions, delete unsused functions
-// TODO remove numbers
-// TODO solver
+// TODO put type outside cell, join recursive functions (generator can just reset the board, call the solver and then set all to final)
+// TODO improve squares
+// TODO solver::getAllSolutions, makePuzzle
+// (maybe) TODO class boardcreator? operator[] that receives coords on class sudoku?
+
+/* =========================================== */
+/* ================ AUXILIARY ================ */
+/* =========================================== */
 
 constexpr int g_size = 9;
+constexpr std::array<int, g_size+1> g_values = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; // 0 is here for padding
+
+struct Coords {
+    int _x;
+    int _y;
+    Coords(int x, int y) : _x(x), _y(y) {};
+};
+
+void shuffle(std::vector<int>& vec) {
+    static std::mt19937 random{ static_cast<std::mt19937::result_type>(std::time(nullptr)) };
+    std::shuffle(vec.begin(), vec.end(), random);
+}
+
+static std::string columnSeparator() {
+    std::string s = " ";
+    for (int i=0; i<(2*g_size+3*2-1); i++) s += "-";
+    s += " \n";
+    return s;
+}
+
+/* =========================================== */
+/* ============== SUDOKU CLASS =============== */
+/* =========================================== */
 
 class Sudoku {
-    struct Num {
-        int _value;
-        int _x;
-        int _y;
-        int _square;
 
-        Num(int val, int x, int y) : _value(val), _x(x), _y(y), _square((y/3)*3+(x/3)) {};
-        friend std::ostream& operator<<(std::ostream& out, const Num& num) {
-            return out << num._value;
+    /* ================ CELL ================= */
+
+    struct Cell {
+        enum Type {final, draft};
+
+        int _value;
+        Type _type;
+
+        Cell(int val) : _value(val), _type(Type::draft) {};
+        friend std::ostream& operator<<(std::ostream& out, const Cell& cell) {
+            return out << cell._value;
         }
     };
 
+    /* ======================================= */
+
 private:
-    Num* _columns[g_size][g_size];
+    Cell* _columns[g_size][g_size];
 
 public:
+
+    /* ============== CONSTRUCTORS =============== */
+
     Sudoku() {
         for (int x=0; x < g_size; x++) {
             for (int y=0; y < g_size; y++) {
-                Num* num = new Num(0, x, y);
-                _columns[x][y] = num;
+                Cell* cell = new Cell(0);
+                _columns[x][y] = cell;
             }
         }
         randomizeBoardRecursive();
+        makePuzzle();
+    }
+
+    Sudoku(const Sudoku& sudoku) {
+        Cell* copy;
+        for (int x=0; x < g_size; x++)
+            for (int y=0; y < g_size; y++) {
+                copy = sudoku._columns[x][y];
+                Cell* cell = new Cell(copy->_value);
+                cell->_type = copy->_type;
+                _columns[x][y] = cell;
+            }
     }
 
     ~Sudoku() {
@@ -43,49 +92,65 @@ public:
                 delete _columns[x][y];
     }
 
-    std::vector<int> generateNumPool() {
-        static std::mt19937 mt{ static_cast<std::mt19937::result_type>(std::time(nullptr)) };
-        std::vector<int> numPool = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // FIXME should it be a vector?
-        std::shuffle(numPool.begin(), numPool.end(), mt);
-        return numPool;
+    /* ============== CELL HANDLING ============== */
+    
+    int getCellValue (const Coords& coords) const {
+        return _columns[coords._x][coords._y]->_value;
     }
 
-    bool inRow(int row, int value) {
-        for (int i=0; i < g_size; i++)
-            if (_columns[i][row]->_value == value) return false;
-        return true;
+    static Coords getNextCell (const Coords& coords) {
+        int nextX = (coords._x+1) % 9;
+        int nextY = coords._y + (coords._x+1)/9; // change row if necessary
+        return Coords{ nextX, nextY };
     }
 
-    bool inColumn(int column, int value) {
-        for (int i=0; i < g_size; i++)
-            if (_columns[column][i]->_value == value) return false;
-        return true;
+    bool outOfBounds(const Coords& coords) const { return coords._y >= g_size; }
+
+    bool isDraftCell(const Coords& coords) const {
+        return _columns[coords._x][coords._y]->_type == Cell::Type::draft;
     }
 
-    bool inSquare(int square, int value) {
-        for (int i=0; i < g_size; i++)
-            if (_columns[(square%3)*3+(i%3)][(square/3)*3+(i/3)]->_value == value) return false;
-        return true;
+    void setDraftCell(const Coords& coords, int value) {
+        Cell* cell = _columns[coords._x][coords._y];
+        if (cell->_type == Cell::Type::draft) // final cells cannot be altered
+            cell->_value = value;
     }
 
-    bool isPossibleValue(int x, int y, int value) {
-        int square_x = (x/3)*3, square_y = (y/3)*3;
+    void setFinalCell(const Coords& coords) {
+        _columns[coords._x][coords._y]->_type = Cell::Type::final;
+    }
+
+    /* ============= RANDOMIZE BOARD ============= */
+
+    bool isPossibleValue(const Coords& coords, int value) const {
+        int x = coords._x, y = coords._y;
+        int square_x = (x/3)*3;
+        int square_y = (y/3)*3;
+
+        if (value < 1 || value > g_size) return false; // 0 is not valid
         for (int i=0; i < g_size; i++) {
             // check row
-            if (_columns[i][y]->_value == value) return false;
+            if (_columns[i][y]->_value == value && i != x)
+                return false;
+
             // check column
-            if (_columns[x][i]->_value == value) return false;
+            if (_columns[x][i]->_value == value && i != y)
+                return false;
+
             // check square
-            if (_columns[square_x+(i%3)][square_y+(i/3)]->_value == value) return false;
+            if (_columns[square_x+(i%3)][square_y+(i/3)]->_value == value
+                && !(square_x+(i%3)==x && square_y+(i/3)==y))
+                return false;
         }
         return true;
     }
 
-    std::vector<int> getPossibleValues(int x, int y) {
-        std::vector<int> values = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; // 0 is here for padding
+    std::vector<int> getPossibleValues(const Coords& coords) const {
+        int x = coords._x, y = coords._y;
+        std::vector<int> values = {g_values.begin(), g_values.end()}; // 0 is here for padding
         int square_x = (x/3)*3, square_y = (y/3)*3; // corresponding 3x3 square
         
-        // search for invalid numbers
+        // search for invalid values
         for (int i=0; i < g_size; i++) {
             // check row
             values[_columns[i][y]->_value] = 0;
@@ -102,31 +167,112 @@ public:
         return values;
     }
 
-    void shuffle(std::vector<int>& vec) {
-        static std::mt19937 mt{ static_cast<std::mt19937::result_type>(std::time(nullptr)) };
-        std::shuffle(vec.begin(), vec.end(), mt);
-    }
+    bool randomizeBoardRecursive(const Coords& currCell = Coords{0,0}) {
+        int x = currCell._x, y = currCell._y;
 
-    bool randomizeBoardRecursive(int x = 0, int y = 0) {
+        // Check if reached end of board -> all done
+        if (outOfBounds(currCell)) return true;
+
         // Find possible values
-        std::vector<int> valuePool = getPossibleValues(x, y);
+        std::vector<int> valuePool = getPossibleValues(currCell);
         shuffle(valuePool);
         
-        while (!valuePool.empty()) { //FIXME
-            // Try value
+        while (!valuePool.empty()) {
             int val = valuePool.back();
-            _columns[x][y]->_value = val;
+            setDraftCell(currCell, val);
 
-            // If this was the last position -> all done
-            if (y == 8 && x == 8) return true; 
+            // Try building board with this value set
+            bool isValidBoard = randomizeBoardRecursive( getNextCell(currCell) );
 
-            // If not, try building board from here
-            int nextX = (x+1) % 9, nextY = y + (x+1)/9; // move on to next position, change row if necessary
-            bool isValidBoard = randomizeBoardRecursive(nextX, nextY); 
-
-            // Cannot build valid board with this value -> try next valuw
+            // Cannot build valid board with this value -> try next value
             if (!isValidBoard) {
-                _columns[x][y]->_value = 0;
+                setDraftCell(currCell, 0);
+                valuePool.pop_back();
+            }
+
+            // Valid board found -> all done
+            else {
+                setFinalCell(currCell);
+                return true;
+            }
+        }
+
+        // No possible value -> backtrack and reassign previous values
+        return false;
+    }
+
+    /* =============== MAKE PUZZLE ================ */
+
+    // TODO remove some values, maybe option to choose difficulty
+    void makePuzzle() {
+        _columns[0][0]->_value = 0;
+        _columns[0][0]->_type = Cell::Type::draft;
+        /*for (int x=0; x < g_size; x++) {
+            for (int y=0; y < g_size; y++) {
+                _columns[x][y] = 0;
+            }
+        }*/
+    }
+
+    /* ================== PRINT ================== */
+
+    friend std::ostream& operator<<(std::ostream& out, const Sudoku& sudoku) {
+        out << "\n\n";
+        for (int y=0; y < g_size; y++) {
+            if (y%3 == 0) out << columnSeparator();
+            for (int x=0; x < g_size; x++) {
+                Cell* cell = sudoku._columns[x][y];
+                if (x%3 == 0) out << "| ";
+                out << *cell << ' ';
+            }
+            out << "|\n";
+        }
+        out << columnSeparator();
+        out << "\n\n";
+        return out;
+    }
+    
+};
+
+/* ================= SOLVER ================== */
+
+namespace SudokuSolver {
+
+    /* Similiar to Sudoku::getNextCell, but looks for next cell to fill */
+    Coords getNextDraftCell(Sudoku& puzzle, const Coords& coords) {
+        Coords nextCoords{coords._x, coords._y};
+        
+        do {
+            nextCoords._y += (nextCoords._x+1)/9;
+            nextCoords._x = (nextCoords._x+1) % 9;
+            
+            if (puzzle.outOfBounds(nextCoords)) return nextCoords; // check if reached end of board
+        } while (!puzzle.isDraftCell(nextCoords));
+        
+        return nextCoords;
+    }
+
+    /* Similiar to Sudoku::randomizeBoardRecursive, but only fills draft cells */
+    bool solveRecursive(Sudoku& puzzle, const Coords& currCell = Coords{0,0}) {
+        int x = currCell._x, y = currCell._y;
+
+        // Check if reached end of board -> all done
+        if (puzzle.outOfBounds(currCell)) return true;
+
+        // Find possible values
+        std::vector<int> valuePool = puzzle.getPossibleValues(currCell);
+        shuffle(valuePool); // probably more efficient on average
+        
+        while (!valuePool.empty()) {
+            int val = valuePool.back();
+            puzzle.setDraftCell(currCell, val);
+
+            // Try building board with this value set
+            bool isValidBoard = solveRecursive( puzzle, getNextDraftCell(puzzle, currCell) );
+
+            // Cannot build valid board with this value -> try next value
+            if (!isValidBoard) {
+                puzzle.setDraftCell(currCell, 0);
                 valuePool.pop_back();
             }
 
@@ -138,32 +284,46 @@ public:
         return false;
     }
 
-    static std::string columnSeparator() {
-        std::string s = " ";
-        for (int i=0; i<(2*g_size+3*2-1); i++) s += "-";
-        s += " \n";
-        return s;
+    void solve(Sudoku& puzzle) { 
+        //puzzle.setDraftCell(Coords{0,0}, 1);
+        Coords firstCell = getNextDraftCell(puzzle, Coords{-1, 0}); // find first empty cell
+        solveRecursive(puzzle, firstCell);
     }
 
-    friend std::ostream& operator<<(std::ostream& out, const Sudoku& sudoku) {
-        out << "\n\n";
-        for (int y=0; y < g_size; y++) {
-            if (y%3 == 0) out << Sudoku::columnSeparator();
-            for (int x=0; x < g_size; x++) {
-                Num* num = sudoku._columns[x][y];
-                if (x%3 == 0) out << "| ";
-                out << *num << ' ';
+    bool isSolution(const Sudoku& puzzle, const Sudoku& solution) {
+        Coords coords{0,0};
+        int cellVal;
+        for (coords._x=0; coords._x < g_size; coords._x++) {
+            for (coords._y=0; coords._y < g_size; coords._y++) {
+                cellVal = solution.getCellValue(coords);
+
+                // check that it is the same puzzle (same fixed cells)
+                if (!puzzle.isDraftCell(coords) && puzzle.getCellValue(coords) != cellVal)
+                    return false;
+
+                // check that there are no invalid values
+                if (!solution.isPossibleValue(coords, cellVal))
+                    return false;
             }
-            out << "|\n";
         }
-        out << Sudoku::columnSeparator();
-        out << "\n\n";
-        return out;
+        return true;
     }
-};
+}
+
+/* =========================================== */
+/* ================== MAIN =================== */
+/* =========================================== */
 
 int main() {
-    Sudoku* sudoku = new Sudoku;
-    std::cout << *sudoku;
+    Sudoku puzzle = Sudoku();
+
+    Sudoku solved = Sudoku(puzzle); // make a copy to keep the original puzzle
+    SudokuSolver::solve(solved);
+    std::cout << puzzle;
+    std::cout << solved;
+
+    if (SudokuSolver::isSolution(puzzle, solved)) std::cout << "SOLVED!\n";
+    else std::cout << "FAILED :(\n";
+
     return 0;
 }
